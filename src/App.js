c4ecfa9b8cefdfc0dd69c3d171488a8423b6a721
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-09-12 21:11:13 +0200

    Add new imports in App/js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ import Trips from './components/views/Trips/TripsContainer';
 // TODO - import other views
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
+import Regions from './components/views/Regions/RegionsContainer';
+import Countries from './components/views/Countries/CountriesContainer';
+import Country from './components/views/Country/CountryContainer'
 
 import parseTrips from './utils/parseTrips';
 import {setMultipleStates} from './redux/globalRedux';
 
-import Regions from './components/views/Regions/Regions';
-import Countries from './components/views/Countries/Countries';
 
 class App extends React.Component {
   static propTypes = {
@@ -46,6 +47,8 @@ class App extends React.Component {
             {/* TODO - add more routes for other views */}
             <Route exact path='/countries' component={Countries} />
             <Route exact path='/regions' component={Regions} />
+            <Route exact path='/trips/id:' component={Trips} />
+            <Route exact path='/country/id:' component={Country} />
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
           </Switch>
