3a33b02271c3b912be5e19c455f0957aa51c5b58
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-09-13 22:11:21 +0200

    Add props in ListOptions

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -11,11 +11,18 @@ const createActionName = name => `app/${reducerName}/${name}`;
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 // TODO - add other action types
-export const CHANGE_DURATION = createActionName('CHANGE_DURATION')
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+
+export const ADD_TAG = createActionName('ADD_TAG');
+
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
+export const addtag = payload => ({ payload, type: ADD_TAG});
+export const removetag = payload => ({ payload, type: REMOVE_TAG});
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION});
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -25,6 +32,16 @@ export default function reducer(statePart = [], action = {}) {
         ...statePart,
         searchPhrase: action.payload,
       };
+      case ADD_TAG:
+        return {
+          ...statePart,
+          tags: [...statePart.tags, action.payload],
+        };
+      case REMOVE_TAG:
+        return {
+          ...statePart,
+          tags: statePart.tags.filter(tag => tag !== action.payload),
+        };
       case CHANGE_DURATION:
         return {
           ...statePart,
