26c2397b1d10a0662b7cd252d7edda649ff63531
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-11-01 21:08:06 +0100

    Add new code according to the task

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Row, Col } from 'react-flexbox-grid';
+import OrderSummary from '../../common/OrderSummary/OrderSummary';
+
+
+
+<Row>
+    <Col xs={12}>
+        <OrderSummary />
+    </Col>
+</Row>
---
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import styles from './OrderSummary.module.scss';
+//import { Row, Col } from 'react-flexbox-grid';
+
+<h2 className={styles.components}>"Total: <strong>$12,345</strong>"</h2>
