e9d55f6ce757efd313dc10eee64bc8a415e6515a
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-11-08 20:56:30 +0100

    Add OrderOption.js to the project

---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './OrderOption.module.scss';
+//import PropTypes from 'prop-types';
+
+const OrderOption = (
+    <div className={styles.component}>
+        <h3 className={styles.title}>Car Rental</h3>
+    </div>
+)
+
+export default OrderOption;
