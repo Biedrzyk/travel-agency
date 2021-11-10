eeb1e497fe9de2180e9f02b6a49ebdf222414378
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-11-10 21:16:44 +0100

    Add props in OptionNumber.js

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -5,7 +5,7 @@ import {formatPrice} from '../../../utils/formatPrice';
 
 
 
-export const OrderOptionNumber = (currentValue, limits, setOptionValue) => {
+export const OrderOptionNumber = (currentValue, limits, setOptionValue, price) => {
     <div className={styles.number}>
       <input
       className={styles.input}
@@ -15,6 +15,15 @@ export const OrderOptionNumber = (currentValue, limits, setOptionValue) => {
       max={limits.max}
       onChange={event => setOptionValue(event.currentTarget.value)}
       >
-      </input>
+      </input> {` Price: ${formatPrice(price)}`}
     </div>
   };
+
+  OrderOptionNumber.propTypes = {
+    limits: PropTypes.object,
+    currentValue: PropTypes.any,
+    setOptionValue: PropTypes.func,
+    price: PropTypes.string,
+  };
+  
+  export default OrderOptionNumber;
