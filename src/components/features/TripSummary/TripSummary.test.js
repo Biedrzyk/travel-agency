50a94c1fce9aa54179f686717d290c43c74bb04b
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-11-17 22:54:54 +0100

    Still fighting with the tests

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -3,10 +3,26 @@ import { shallow } from 'enzyme';
 import TripSummary from './TripSummary';
 
 describe('Component TripSummary', () => {
-  it('should render without crashing', () => {
-    const expectedAdress = 'trip/abc';
+  it('should generate without crashing', () => {
+    const component = shallow(<TripSummary
+      id='abc'
+      image='image.jpg'
+      name='bob' />);
+    expect(component).toBeTruthy();
+  });
+  it('should generate right address', () => {
+    const expectedAdress = '/trip/abc';
     const expectedId = 'abc';
     const component = shallow(<TripSummary id={expectedId} />);
     expect(component.find('Link').prop('to')).toEqual(expectedAdress);
+  });
+
+  it('should render correct alt and image', () => {
+    const expectedAlt = 'bob';
+    const expectedImage = 'image.jpg';
+    const component = shallow(<TripSummary image={expectedImage} name={expectedAlt} />);
+
+    expect(component.find('img').prop('src')).toEqual(expectedImage);
+    expect(component.find('img').prop('alt')).toEqual(expectedAlt);
   });
 });
