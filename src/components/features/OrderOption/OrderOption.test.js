66d048e416850d2457644aeb55ee129dd3dca4bf
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-11-21 22:01:11 +0100

    Corrected few typos

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import OrderOption from './OrderOption';
 
-describe('Component TripSummary', () => {
-  it('should generate without crashing', () => {
+describe('Component OrderOption', () => {
+  it('should render without crashing', () => {
     const component = shallow(<OrderOption
       type='lolo'
       name='bobo' 
     />);
     expect(component).toBeTruthy();
   });
+
+  it('should return empty object if called without required props', () => {
+    const component = shallow(<OrderOption />);
+    expect(component).toEqual({});
+  });
+
 });
