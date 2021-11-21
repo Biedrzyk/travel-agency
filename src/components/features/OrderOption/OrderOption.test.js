89bc640df6aa47ca49ecb3cb866dc842b82a7bb0
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-11-21 22:46:48 +0100

    Still not working

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -8,17 +8,14 @@ describe('Component OrderOption', () => {
   it('should render without crashing', () => {
     const component = shallow(<OrderOption name={expectedName} type={expectedType} />);
     expect(component).toBeTruthy();
-    console.log(component.debug());
   });
-
   it('should return empty object if called without required props', () => {
     const component = shallow(<OrderOption />);
     expect(component).toEqual({});
   });
-
   it('should have title from props name', () => {
     const component = shallow(<OrderOption name={expectedName} type={expectedType} />);
     expect(component.find('.title').text()).toEqual(expectedName);
   });
-
 });
+
