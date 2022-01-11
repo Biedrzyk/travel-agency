c776e36d10312ee23b6f18c46cd796c053b0757e
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2022-01-11 22:37:07 +0100

    Fixed problem with digits

--- src/components/layout/Hero/Hero.js
+++ src/components/layout/Hero/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Hero.module.scss';
 import PropTypes from 'prop-types';
 import HappyHourAd from '../../features/HappyHourAd/HappyHourAd';
-import DaysToSummer from '../../features/DaysToSummer/DaysToSummer';
+import DaysToSummer from '../../features/DaysToSUmmer/DaysToSummer';
 
 const Hero = ({variant = '', titleText, imageSrc, ...otherProps}) => (
   <div {...otherProps} className={styles.component + variant.split(' ').map(name => ' ' + (styles[name] || name)).join('')}>
